efab3935300bc957174eed0db5de82fbf85527d2
김제혁(유알) <jh.kim@smartstudygames.com>
2025-06-15 13:14:06 +0900

    UIManager #2

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -7,6 +7,48 @@ public class UIManager
     int _order = 0;
 
     Stack<UI_Popup> _popupStack = new Stack<UI_Popup>();
+    UI_Scene _sceneUI = null;
+    public GameObject Root
+    {
+        get
+        {
+            var root = GameObject.Find("@UI_Root");
+            if (root == null)
+                root = new GameObject { name = "@UI_Root" };
+
+            return root;
+        }
+    }
+    public void SetCanvas(GameObject go, bool sort = true)
+    {
+        Canvas canvas = go.AddMissingComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.overrideSorting = true;
+
+        if (sort)
+        {
+            canvas.sortingOrder = _order;
+            _order++;
+        }
+        else
+        {
+            canvas.sortingOrder = 0;
+        }
+    }
+
+    public T ShowSceneUI<T>(string name = null) where T : UI_Scene
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Popup/{name}");
+        T sceneUI = go.AddMissingComponent<T>();
+        _sceneUI = sceneUI;
+
+        go.transform.SetParent(Root.transform);
+
+        return sceneUI;
+    }
 
     public T ShowPopupUI<T>(string name = null) where T : UI_Popup
     {
@@ -17,7 +59,7 @@ public class UIManager
         T popup = go.AddMissingComponent<T>();
         _popupStack.Push(popup);
 
-        _order++;
+        go.transform.SetParent(Root.transform);
 
         return popup;
     }
